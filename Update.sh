9389a3cc2c5b4c80a78199fbff1ef398e55586f4
Rkcoder0425 <150472171+Rkcoder0425@users.noreply.github.com>
2024-02-05 12:29:23 +0500

    Create Update.sh

---
+++ Update.sh
@@ -0,0 +1,14 @@
+figlet WELCOME BACK
+
+echo -e "Checking system.\n Optimiaing system battery>
+echo -e "<======================updating system======>
+
+apt update -y
+echo -e"System updated successfully.\n Checking fir u>
+echo -e "<======================upgading system======>
+
+apt upgrade -y
+echo -e"System upgraded successfully"
+echo -e "You are ready to attack."
+figlet RAJA
+echo -e "   <------------created by Raja------------>"
